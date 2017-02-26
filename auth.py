7bf46cf83000eb6830d9e8dc4f5f11aadb4176bb
Stanley Ng <sheingjing.ng@drake.edu>
2017-02-26 03:58:09 -0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,8 +1,9 @@
 
 class TwitterAuth:
-	consumer_key="asFdELVBas6oIVtyYWc7VmfuC"
-	consumer_secret="DDMg3n35wXHhBlI4PCnPRre4CQk82805eC4PJe8c9KjsdTJBQd"
+	consumer_key="asFdELVBas6oIVtXXXXXXXXXXX"
+	consumer_secret="DDMg3n35wXHhBlI4PCnPRre4CQk8280XXXXXXXXXXXXX"
 
-	access_token="834801524815646722-Us7I4ybTdZMEQ5TnzStMV3MZst6dH4w"
-	access_token_secret="f5N6avq6GmDyhPe41UYUvy113CFE1o5m9gXCYM4hHrhCh"
+	access_token="834801524815646722-Us7I4ybTdZXXXXXXXXXXXXX"
+	access_token_secret="f5N6avq6GmDyhPe41UYUvyXXXXXXXXXXXXXX"
+#Please Change the above Twitter API credentials
 
